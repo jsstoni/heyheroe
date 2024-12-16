433f55c7388f966f1b6f17ccb50a605494013d2c
bannednull <bannednull@gmail.com>
2024-12-16 12:59:51 -0300

    mostrar sub servicio

--- src/app/(home)/services/[id]/[sub]/page.tsx
+++ src/app/(home)/services/[id]/[sub]/page.tsx
@@ -1,3 +1,36 @@
-export default function Sub() {
-  return <h1>Sub servicio</h1>;
+import prisma from '@/lib/db';
+
+interface PropsParams {
+  params: Promise<{ sub: string }>;
+}
+
+export const dynamic = 'force-static';
+export const revalidate = 60;
+
+export default async function Sub({ params }: PropsParams) {
+  const { sub } = await params;
+  const data = await prisma.subServices.findUnique({
+    where: {
+      id: +sub,
+    },
+    select: {
+      name: true,
+      service: {
+        select: {
+          name: true,
+        },
+      },
+    },
+  });
+
+  if (!data) {
+    return <h1>Sub service not found</h1>;
+  }
+
+  return (
+    <div className="bg-indigo-400 p-14 text-center">
+      <h1 className="text-5xl font-bold text-white">{data.service.name}</h1>
+      <h2 className="text-3xl font-bold text-zinc-600">{data.name}</h2>
+    </div>
+  );
 }
