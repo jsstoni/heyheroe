c66c900630818cc2c4b82a83c09db13b6189193b
bannednull <bannednull@gmail.com>
2024-12-16 23:03:19 -0300

    componente textarea

---
+++ src/components/ui/textarea.tsx
@@ -0,0 +1,45 @@
+import { cn } from '@/lib/utils';
+import * as React from 'react';
+import { FieldError, FieldErrorsImpl, Merge } from 'react-hook-form';
+
+type TextareaError =
+  | FieldError
+  | Merge<FieldError, FieldErrorsImpl<any>>
+  | string
+  | undefined;
+
+interface TextareaProps extends React.ComponentProps<'textarea'> {
+  error?: TextareaError;
+}
+
+const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
+  ({ className, error, ...props }, ref) => {
+    const errorMessage =
+      typeof error === 'string'
+        ? error
+        : error && 'message' in error
+          ? (error.message as string)
+          : null;
+
+    return (
+      <div>
+        <textarea
+          className={cn(
+            'w-full rounded-md border px-3 py-1.5',
+            errorMessage && 'border-red-500',
+            className
+          )}
+          ref={ref}
+          {...props}
+        />
+        {errorMessage && (
+          <p className="mt-1 text-sm text-red-500">{errorMessage}</p>
+        )}
+      </div>
+    );
+  }
+);
+
+Textarea.displayName = 'Textarea';
+
+export { Textarea };
