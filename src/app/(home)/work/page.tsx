2f3b368ea0215dfe7ab13016687363e10f4d5a82
bannednull <bannednull@gmail.com>
2024-12-23 20:24:22 -0300

    cache en los datos

--- src/app/(home)/work/page.tsx
+++ src/app/(home)/work/page.tsx
@@ -1,32 +1,41 @@
 import prisma from '@/lib/db';
+import { cache } from 'react';
 import { WorkService } from './_components/work';
 
-export const dynamic = 'force-dynamic';
-export const revalidate = 60;
-
-export default async function Work() {
-  const services = await prisma.services.findMany({
-    orderBy: {
-      name: 'asc',
-    },
-    where: {
-      active: true,
-    },
-    include: {
-      subServices: {
-        where: {
-          active: true,
-        },
-        select: {
-          id: true,
-          name: true,
-        },
-        orderBy: {
-          name: 'asc',
+const getData = cache(async () => {
+  'use server';
+  try {
+    const services = await prisma.services.findMany({
+      orderBy: {
+        name: 'asc',
+      },
+      where: {
+        active: true,
+      },
+      include: {
+        subServices: {
+          where: {
+            active: true,
+          },
+          select: {
+            id: true,
+            name: true,
+          },
+          orderBy: {
+            name: 'asc',
+          },
         },
       },
-    },
-  });
+    });
+    return services;
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
+});
+
+export default async function Work() {
+  const services = await getData();
 
   return (
     <>
