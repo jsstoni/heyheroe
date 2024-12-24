8ec0585ef43f6ec21f784e8e5d607b1b98df0faf
bannednull <bannednull@gmail.com>
2024-12-24 12:40:56 -0300

    adaptador de prisma para almacenar usuarios

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,7 +1,18 @@
-import NextAuth from 'next-auth';
+import { PrismaAdapter } from '@auth/prisma-adapter';
+import NextAuth, { DefaultSession } from 'next-auth';
 import Google from 'next-auth/providers/google';
+import prisma from './db';
+
+declare module 'next-auth' {
+  interface Session {
+    user: {
+      phoneNumber?: string;
+    } & DefaultSession['user'];
+  }
+}
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  adapter: PrismaAdapter(prisma),
   providers: [Google],
   trustHost: true,
   session: {
