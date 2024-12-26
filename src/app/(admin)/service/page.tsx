d7f178c1aa208c8a32f3d8abe740011528150ad1
bannednull <bannednull@gmail.com>
2024-12-26 14:18:50 -0300

    listar los servicios del usuario

--- src/app/(admin)/service/page.tsx
+++ src/app/(admin)/service/page.tsx
@@ -1,15 +1,69 @@
+import { Heading } from '@/components/heading';
+import { auth } from '@/lib/auth';
+import prisma from '@/lib/db';
 import Link from 'next/link';
+import { cache } from 'react';
 
-export default function Service() {
+const getData = cache(async () => {
+  const session = await auth();
+  if (!session || !session.user?.id) return [];
+
+  try {
+    const services = prisma.professional.findMany({
+      where: {
+        userId: +session.user.id,
+      },
+      include: {
+        service: {
+          select: {
+            name: true,
+            service: {
+              select: {
+                name: true,
+              },
+            },
+          },
+        },
+      },
+    });
+
+    return services;
+  } catch (error) {
+    console.log(error);
+    return [];
+  }
+});
+
+export default async function Service() {
+  const services = await getData();
+
   return (
     <>
-      <h1 className="bg-zinc-100 p-4 text-xs">Mis Servicios</h1>
-      <div className="p-5">
+      <Heading title="Mis Servicios" sub="Administra tus servicios y precios" />
+
+      <div className="mt-4 grid gap-4 md:grid-cols-3">
+        {services.map((service) => (
+          <div
+            className="flex items-start rounded-md border p-4 shadow-sm"
+            key={service.id}
+          >
+            <div className="flex flex-col">
+              <p>{service.service.service.name}</p>
+              <p className="text-sm text-zinc-400">{service.service.name}</p>
+            </div>
+            <strong className="ml-auto text-green-600">
+              ${service.price}/hora
+            </strong>
+          </div>
+        ))}
         <Link
-          className="rounded-md border border-orange-500 p-2 text-orange-500"
+          className="rounded-md border-2 border-dashed p-2 text-center"
           href="/service/create"
         >
-          Agregar servicio
+          <strong>Agregar nuevo servicio</strong>
+          <span className="block text-zinc-400">
+            Añade un nuevo servicio a tu catálogo
+          </span>
         </Link>
       </div>
     </>
