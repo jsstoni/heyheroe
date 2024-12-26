4e6498ea791048a200c7f12e10c5f25a3259ffe4
bannednull <bannednull@gmail.com>
2024-12-26 14:08:57 -0300

    layout admin

--- src/app/(admin)/layout.tsx
+++ src/app/(admin)/layout.tsx
@@ -6,23 +6,22 @@ export default function Layout({ children }: { children: React.ReactNode }) {
     <>
       <NavBar />
 
-      <main className="container relative mx-auto h-[calc(100vh-65px)] border-x">
-        <aside className="absolute inset-y-0 flex w-[260px] flex-col gap-4 border-r bg-white">
-          <nav className="flex flex-col">
+      <main className="relative mt-[65px]">
+        <div className="bg-zinc-200 py-2.5">
+          <nav className="container mx-auto flex items-center gap-2">
             {menu.map((item) => (
               <ActiveLink
-                className="flex items-center gap-2 px-4 py-2.5 hover:bg-zinc-200 hover:text-orange-500"
+                className="flex items-center gap-2 px-4 py-1"
                 href={item.href}
                 key={item.name}
               >
-                <img src={item.icon} alt={item.name} className="size-6" />
                 {item.name}
               </ActiveLink>
             ))}
           </nav>
-        </aside>
+        </div>
 
-        <section className="mt-[65px] md:ml-[260px]">{children}</section>
+        <section className="container mx-auto py-4">{children}</section>
       </main>
     </>
   );
@@ -32,11 +31,9 @@ const menu = [
   {
     name: 'Perfil',
     href: '/admin',
-    icon: '/svgs/icon-entrenador.svg',
   },
   {
     name: 'Mis Servicios',
     href: '/service',
-    icon: '/svgs/icon-servicio.svg',
   },
 ];
