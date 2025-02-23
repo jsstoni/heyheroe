604e7bf5dd830569185205ee2c422c45f63dedaf
bannednull <bannednull@gmail.com>
2025-02-23 16:52:06 -0300

    eliminando uso de eslint-plugin-tailwindcss

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -14,7 +14,6 @@ const eslintConfig = [
   ...compat.extends(
     'next/core-web-vitals',
     'next/typescript',
-    'plugin:tailwindcss/recommended',
     'plugin:@typescript-eslint/recommended',
     'prettier'
   ),
