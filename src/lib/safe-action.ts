75df480bd845d4c623ae4546a524fdc97e9b5109
bannednull <bannednull@gmail.com>
2025-02-23 19:39:59 -0300

    next-safe-action

---
+++ src/lib/safe-action.ts
@@ -0,0 +1,46 @@
+import {
+  createSafeActionClient,
+  DEFAULT_SERVER_ERROR_MESSAGE,
+} from 'next-safe-action';
+import { z } from 'zod';
+import { auth } from './auth';
+
+export const actionClient = createSafeActionClient({
+  handleServerError(e) {
+    console.error('Action error:', e.message);
+
+    if (e instanceof Error) {
+      return e.message;
+    }
+
+    return DEFAULT_SERVER_ERROR_MESSAGE;
+  },
+  defineMetadataSchema() {
+    return z.object({
+      name: z.string(),
+    });
+  },
+}).use(async ({ next, clientInput, metadata }) => {
+  const result = await next({ ctx: {} });
+  if (process.env.NODE_ENV === 'development') {
+    console.log(`Input -> ${JSON.stringify(clientInput)}`);
+    console.log(`Result -> ${JSON.stringify(result.data)}`);
+    console.log(`Metadata -> ${JSON.stringify(metadata)}`);
+
+    return result;
+  }
+
+  return result;
+});
+
+export const authActionClient = actionClient.use(async ({ next }) => {
+  const session = await auth();
+
+  if (!session?.user) throw new Error('Unauthorized');
+
+  return next({
+    ctx: {
+      user: session.user.id,
+    },
+  });
+});
