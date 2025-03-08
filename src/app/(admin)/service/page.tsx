1f017c61760c40d1f090394a85546fc7257f41b0
bannednull <bannednull@gmail.com>
2025-03-08 18:28:46 -0300

    mis servicios

--- src/app/(admin)/service/page.tsx
+++ src/app/(admin)/service/page.tsx
@@ -1,17 +1,20 @@
 import { Heading } from '@/components/heading';
 import { auth } from '@/lib/auth';
 import prisma from '@/lib/db';
+import { headers } from 'next/headers';
 import Link from 'next/link';
 import { cache } from 'react';
 
+export const dynamic = 'force-dynamic';
+
 const getData = cache(async () => {
-  const session = await auth();
-  if (!session || !session.user?.id) return [];
+  const session = await auth.api.getSession({ headers: await headers() });
+  if (!session || !session.user.id) return [];
 
   try {
     const services = prisma.professional.findMany({
       where: {
-        userId: +session.user.id,
+        userId: session.user.id,
       },
       include: {
         service: {
@@ -44,20 +47,22 @@ export default async function Service() {
       <div className="mt-4 grid gap-4 md:grid-cols-3">
         {services.map((service) => (
           <div
-            className="flex items-start rounded-md border p-4 shadow-xs"
+            className="flex items-start rounded-md border p-4"
             key={service.id}
           >
             <div className="flex flex-col">
-              <p className="text-xl">{service.service.service.name}</p>
-              <p className="text-sm text-zinc-400">{service.service.name}</p>
+              <h3 className="text-xl font-medium text-gray-600">
+                {service.service.service.name}
+              </h3>
+              <h4 className="text-sm text-gray-400">{service.service.name}</h4>
             </div>
-            <span className="ml-auto text-green-600">
+            <span className="ml-auto text-xs font-medium text-green-600">
               ${service.price}/hora
             </span>
           </div>
         ))}
         <Link
-          className="flex flex-col justify-center rounded-md border-2 border-dashed p-2 text-center hover:border-orange-500"
+          className="flex flex-col justify-center rounded-md border-2 border-dashed p-2 text-center hover:border-amber-500"
           href="/service/create"
         >
           <strong>Agregar nuevo servicio</strong>
