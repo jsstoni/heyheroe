5ae2f2bfcb8a9f225d1749070b18be77ff43a736
bannednull <bannednull@gmail.com>
2025-03-09 01:13:59 -0300

    layout dashboard

--- src/app/(admin)/layout.tsx
+++ src/app/(admin)/layout.tsx
@@ -1,27 +1,29 @@
 import { ActiveLink } from '@/components/active-link';
 import { NavBar } from '@/components/nav';
+import { Clock, DollarSign, Send, User } from 'lucide-react';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
       <NavBar />
 
-      <main className="relative mt-[65px]">
-        <div className="bg-gray-100 py-3 max-sm:px-4">
-          <nav className="container mx-auto flex items-center gap-3">
+      <main className="relative mt-[72px]">
+        <div className="border-b bg-gray-50 max-sm:px-4">
+          <nav className="container mx-auto flex items-center">
             {menu.map((item) => (
               <ActiveLink
-                className="flex items-center gap-2 rounded-full bg-gray-200 px-4 py-1"
+                className="flex items-center gap-2 px-4 py-3.5 first:pl-0 hover:text-amber-400"
                 href={item.href}
                 key={item.name}
               >
+                {item.Icon}
                 {item.name}
               </ActiveLink>
             ))}
           </nav>
         </div>
 
-        <section className="container mx-auto py-4 max-sm:px-4">
+        <section className="container mx-auto py-6 max-sm:px-4">
           {children}
         </section>
       </main>
@@ -33,13 +35,21 @@ const menu = [
   {
     name: 'Perfil',
     href: '/admin',
+    Icon: <User className="size-4" />,
   },
   {
     name: 'Mis Servicios',
     href: '/service',
+    Icon: <Clock className="size-4" />,
   },
   {
     name: 'Solicitudes',
     href: '/request',
+    Icon: <Send className="size-4" />,
+  },
+  {
+    name: 'Finanzas',
+    href: '/finance',
+    Icon: <DollarSign className="size-4" />,
   },
 ];
