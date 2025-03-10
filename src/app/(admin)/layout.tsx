70c9fcec9796ef5151e4968cf4f7d328c3bf08f7
bannednull <bannednull@gmail.com>
2025-03-09 23:10:40 -0300

    se elimina la creacion de servicios

--- src/app/(admin)/layout.tsx
+++ src/app/(admin)/layout.tsx
@@ -1,6 +1,6 @@
 import { ActiveLink } from '@/components/active-link';
 import { NavBar } from '@/components/nav';
-import { Clock, DollarSign, Send, User } from 'lucide-react';
+import { Clock, CreditCard, User } from 'lucide-react';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
@@ -8,8 +8,8 @@ export default function Layout({ children }: { children: React.ReactNode }) {
       <NavBar />
 
       <main className="relative mt-[72px]">
-        <div className="border-b bg-gray-50 max-sm:px-4">
-          <nav className="container mx-auto flex items-center">
+        <div className="border-b max-sm:px-4">
+          <nav className="container mx-auto flex items-center font-medium">
             {menu.map((item) => (
               <ActiveLink
                 className="flex items-center gap-2 px-4 py-3.5 first:pl-0 hover:text-amber-400"
@@ -38,18 +38,13 @@ const menu = [
     Icon: <User className="size-4" />,
   },
   {
-    name: 'Mis Servicios',
-    href: '/service',
-    Icon: <Clock className="size-4" />,
-  },
-  {
     name: 'Solicitudes',
     href: '/request',
-    Icon: <Send className="size-4" />,
+    Icon: <Clock className="size-4" />,
   },
   {
     name: 'Finanzas',
     href: '/finance',
-    Icon: <DollarSign className="size-4" />,
+    Icon: <CreditCard className="size-4" />,
   },
 ];
