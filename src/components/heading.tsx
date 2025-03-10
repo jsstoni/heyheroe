c82b6c5dda50735f92a98a0b4d26fe12be53f54b
bannednull <bannednull@gmail.com>
2025-03-10 19:35:57 -0300

    heading

--- src/components/heading.tsx
+++ src/components/heading.tsx
@@ -1,8 +1,8 @@
 export function Heading({ title, sub }: { title: string; sub?: string }) {
   return (
-    <>
+    <div>
       <h1 className="text-2xl font-medium">{title}</h1>
       {sub && <h2 className="text-sm text-gray-400">{sub}</h2>}
-    </>
+    </div>
   );
 }
