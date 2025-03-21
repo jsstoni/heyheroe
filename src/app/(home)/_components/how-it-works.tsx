f20aa0b64f13e4ccb37bb8dc83422cd49acf13f9
bannednull <bannednull@gmail.com>
2025-03-20 21:37:43 -0300

    color

--- src/app/(home)/_components/how-it-works.tsx
+++ src/app/(home)/_components/how-it-works.tsx
@@ -9,14 +9,16 @@ export default function HowItWorks({ steps }: Props) {
 
       <div className="grid gap-10 md:grid-cols-3">
         {steps.map(({ title, description }, index) => (
-          <div className="relative rounded-md bg-gray-100 p-6" key={index}>
+          <div className="relative rounded-md border bg-white p-6" key={index}>
             <div className="absolute -top-2 -left-2 size-4 border-t-2 border-l-2 border-gray-400" />
             <div className="absolute -right-2 -bottom-2 size-4 border-r-2 border-b-2 border-gray-400" />
 
-            <strong className="text-primary-400 border-primary-400 absolute -top-6 flex size-12 items-center justify-center rounded-xl border bg-white text-4xl">
+            <strong className="text-primary-500 border-primary-400 absolute -top-6 flex size-12 items-center justify-center rounded-xl border bg-white text-4xl">
               {index + 1}
             </strong>
-            <h4 className="my-2 text-2xl font-medium">{title}</h4>
+            <h4 className="text-primary-600 my-2 text-2xl font-medium">
+              {title}
+            </h4>
 
             <p className="text-gray-500">{description}</p>
           </div>
