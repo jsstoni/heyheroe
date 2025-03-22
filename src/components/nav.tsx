0a72d1ded53f3002ce5c13e6063c30d03ef9c129
bannednull <bannednull@gmail.com>
2025-03-22 10:17:19 -0300

    brand

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -6,9 +6,9 @@ export const NavBar = () => {
   return (
     <header className="fixed top-0 z-50 w-full border-b bg-white">
       <nav className="container flex h-[70px] items-center justify-between gap-4 px-4 md:px-0">
-        <Link className="text-2xl font-black" href="/">
-          Hey
-          <span className="text-primary-400">Héroe</span>
+        <Link className="flex items-end gap-1 text-2xl font-black" href="/">
+          <img src="/logo.svg" width="25" height="25" alt="logo" />
+          eyhéroe
         </Link>
 
         <ul className="ml-auto flex items-center justify-between gap-5 font-medium">
