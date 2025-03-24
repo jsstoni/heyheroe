27f8edd15f271a4799a7c0e0fd9c399e86fb490f
bannednull <bannednull@gmail.com>
2025-03-24 19:32:30 -0300

    empty state agregado

--- src/app/(admin)/dashboard/request/page.tsx
+++ src/app/(admin)/dashboard/request/page.tsx
@@ -1,8 +1,8 @@
 import { Heading } from '@/components/heading';
 import { auth } from '@/lib/auth';
 import { formatPrice, relativeDate } from '@/lib/utils';
-import { myRequests } from '#/admin/dashboard/request/data';
-import { Calendar } from 'lucide-react';
+import { getCachedRequests } from '#/admin/dashboard/request/data';
+import { Calendar, File } from 'lucide-react';
 import { headers } from 'next/headers';
 
 export const dynamic = 'force-dynamic';
@@ -14,13 +14,55 @@ const getMyRequests = async () => {
     return [];
   }
 
-  const resquests = await myRequests(session.user.id);
+  const resquests = await getCachedRequests(session.user.id);
   return resquests;
 };
 
-export default async function Request() {
+async function AsyncDataRequest() {
   const resquests = await getMyRequests();
 
+  if (resquests.length < 1) {
+    return (
+      <div className="mt-5 flex flex-col items-center justify-center rounded-lg border border-dashed p-16 text-center">
+        <div className="mb-4 flex h-16 w-16 items-center justify-center rounded-full bg-gray-100">
+          <File className="size-10" />
+        </div>
+        <strong>No tienes solicitudes de servicios</strong>
+        <p className="mx-auto max-w-md text-gray-500">
+          Cuando solicites un servicio, podrás ver su estado y gestionar los
+          detalles aquí.
+        </p>
+      </div>
+    );
+  }
+
+  return (
+    <div className="mt-6 space-y-4">
+      {resquests.map((service) => (
+        <div
+          className="rounded-lg border bg-white p-4 text-gray-400 shadow-sm"
+          key={service.id}
+        >
+          <h3 className="text-xl font-medium text-gray-800">
+            {service.subServices.service.name} - {service.subServices.name}
+          </h3>
+          <p className="flex items-center gap-2 text-sm">
+            <Calendar className="size-3" />
+            {relativeDate(service.createdAt)}
+          </p>
+
+          <p className="my-2">{service.description}</p>
+
+          <span className="text-gray-800">
+            {formatPrice(Number(service.budget))}
+          </span>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default async function Request() {
   return (
     <>
       <Heading
@@ -28,28 +70,7 @@ export default async function Request() {
         sub="Gestiona los servicios que has solicitado"
       />
 
-      <div className="mt-6 space-y-4">
-        {resquests.map((service) => (
-          <div
-            className="rounded-lg border bg-white p-4 text-gray-400 shadow-sm"
-            key={service.id}
-          >
-            <h3 className="text-xl font-medium text-gray-800">
-              {service.subServices.service.name} - {service.subServices.name}
-            </h3>
-            <p className="flex items-center gap-2 text-sm">
-              <Calendar className="size-3" />
-              {relativeDate(service.createdAt)}
-            </p>
-
-            <p className="my-2">{service.description}</p>
-
-            <span className="text-gray-800">
-              {formatPrice(Number(service.budget))}
-            </span>
-          </div>
-        ))}
-      </div>
+      <AsyncDataRequest />
     </>
   );
 }
