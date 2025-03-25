e373d440e5f4ca5737a8c2cfde081cceb80342e1
bannednull <bannednull@gmail.com>
2025-03-25 17:27:08 -0300

    standalone

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   output: 'standalone',
   compress: false,
+  transpilePackages: ['@t3-oss/env-nextjs', '@t3-oss/env-core'],
   // Optional: bring your own cache handler
   //cacheHandler: path.resolve('./cache-handler.mjs'),
   //cacheMaxMemorySize: 0, // Disable default in-memory caching
