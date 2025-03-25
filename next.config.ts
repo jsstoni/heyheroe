0c0e2d0601dce4160f00b24b224f12acab05c4e2
bannednull <bannednull@gmail.com>
2025-03-25 17:00:59 -0300

    standlone

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,22 @@
+//import path from 'path';
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+  output: 'standalone',
+  compress: false,
+  // Optional: bring your own cache handler
+  //cacheHandler: path.resolve('./cache-handler.mjs'),
+  //cacheMaxMemorySize: 0, // Disable default in-memory caching
   eslint: {
     ignoreDuringBuilds: true,
   },
   images: {
-    domains: ['lh3.googleusercontent.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+      },
+    ],
   },
 };
 
