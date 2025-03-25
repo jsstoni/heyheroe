8a23ab7c2d5b8bf2d22149070d4a35183288ffd0
bannednull <bannednull@gmail.com>
2025-03-25 17:05:42 -0300

    variables de entorno

---
+++ src/lib/env/server.ts
@@ -0,0 +1,16 @@
+import { createEnv } from '@t3-oss/env-nextjs';
+import { z } from 'zod';
+
+export const env = createEnv({
+  server: {
+    DATABASE_URL: z
+      .string()
+      .url()
+      .refine((str) => str.startsWith('postgresql://')),
+    BETTER_AUTH_URL: z.string().url(),
+    BETTER_AUTH_SECRET: z.string().min(1),
+    AUTH_GOOGLE_ID: z.string().min(1),
+    AUTH_GOOGLE_SECRET: z.string().min(1),
+  },
+  experimental__runtimeEnv: process.env,
+});
